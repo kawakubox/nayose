c1eb32827bfe6c25ea1bb850426d1d0ac9ed5c5c
kawakubox <shigefumi.kawakubo@gmail.com>
2016-06-26 22:42:30 +0900

    Implements callbacks

--- app/models/company_name.rb
+++ app/models/company_name.rb
@@ -1,3 +1,19 @@
 class CompanyName
   include Neo4j::ActiveNode
+
+  property :text, type: String, constraint: :unique
+
+  before_save do
+    self.text = text.underscore
+  end
+
+  after_save do
+    sanitize.split(/[ ]+/).map { |word| Word.find_or_create(text: word) }
+  end
+
+  private
+
+  def sanitize
+    self.text
+  end
 end
